5c4588ddbd63afd98440ea97006cd20e1835a0b9
Chetan <165123379+mn-chetan@users.noreply.github.com>
2024-10-31 21:59:52 +0530

    Add display method to board class

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+# Board class displays game_board and updates game_board
+class Board
+  # Function to display board on console
+  # Takes a array game_board as an argument
+  def display_board(game_board)
+    (0..game_board.length - 1).each do |i|
+      # For index 3 and 6, wrap display to next line
+      if [3, 6].include?(i)
+        puts
+        puts '-----------'
+      end
+      print "#{i + 1} | "
+    end
+    puts
+  end
+end
